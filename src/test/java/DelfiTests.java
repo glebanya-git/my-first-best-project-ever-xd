ddafc5e0ce46f0e64c88935bd7722f0cd9dc7a08
glebanya-git <glebs.jelagins@gmail.com>
2020-12-03 20:55:43 +0200

    obnovlenie DelfiTests i Testi 03.12.2020 kogda sidel s julej pisal

--- src/test/java/DelfiTests.java
+++ src/test/java/DelfiTests.java
@@ -1,19 +1,33 @@
-
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class DelfiTests {
+
+    private final By ACCEPT_COOKIE_BTN = By.xpath(".//button[@mode = 'primary']");
+
+
     @Test
     public void firstDelfiTest() {
 
         System.setProperty("webdriver.chrome.driver", "C://Users/glebsj/Downloads/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://delfi.lv");
 
+        //  Thread.sleep(10000); - plohoi ton, ne rekomneduetsa ispolzovatj
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(ACCEPT_COOKIE_BTN));
+
+        WebElement acceptBtn = driver.findElement(ACCEPT_COOKIE_BTN);
+        acceptBtn.click();
     }
 
-
-
 }
+
---
+++ src/test/java/Testi.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+
+public class Testi {
+
+
+    private final By NEWS = By.xpath(".//span[@itemprop = 'headline name']");
+
+    @Test
+
+    public void homeworkTvnet() {
+        String aritcleToOpen = "Gaiļezera feldšerei nav taisnība – vakcīnas ir pārbaudītas un maskas nenogalina";
+
+        System.setProperty("webdriver.chrome.driver", "C://Users/glebsj/Downloads/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://tvnet.lv");
+
+        List<WebElement> articles = driver.findElements(NEWS);
+
+        for (int i = 0; i < articles.size(); i++) {
+            System.out.println(i + " " + articles.get(i).getText());
+        }
+        }
+    }
