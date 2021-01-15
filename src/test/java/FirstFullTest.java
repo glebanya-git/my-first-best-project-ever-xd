d6c1804e7fb4b41afaf871c4408e159b013eaf51
glebanya-git <glebs.jelagins@gmail.com>
2021-01-15 17:11:49 +0200

    Dobavlenie package: page; klassa: HomePage; klassa: BaseFunc; klassa: PageObectTest
    13.01.2021 - firstfull test dodelivaem

--- src/test/java/FirstFullTest.java
+++ src/test/java/FirstFullTest.java
@@ -1,32 +1,50 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FirstFullTest {
+
+    private final By ACCEPT_COOKIE_BTN = By.xpath(".//div[contains(@class, 'button cookie')]");
     private final By ARTICLE = By.tagName("article");
     private final By ARTICLE_TITLE = By.xpath(".//span[@itemprop = 'headline name']");
     private final By COMMENTS_COUNT = By.xpath(".//span[contains(@class, 'list-article__comment')]");
-
     private final By ARTICLE_PAGE_TITLE = By.xpath(".//h1[@itemprop = 'headline name']");
+    private final Logger LOGGER = LogManager.getLogger(FirstFullTest.class);
 
-   @Test
+            @Test
 
-   public void titleCommentsCheck() {
+    public void titleCommentsCheck() {
+
+       LOGGER.info("This test is checking titles and comments on tvnet page");
        System.setProperty("webdriver.chrome.driver", "C://Users/glebsj/Downloads/chromedriver.exe");
+
+       LOGGER.info("We are opening driver window and maximizing it");
        WebDriver driver = new ChromeDriver();
        driver.manage().window().maximize();
+
+       LOGGER.info("Opening tvnet main page");
        driver.get("http://tvnet.lv");
 
-       // Find article
+       LOGGER.info("Clicking the Accept cookies btn");
+       WebDriverWait wait = new WebDriverWait(driver, 10);
+       wait.until(ExpectedConditions.visibilityOfElementLocated(ACCEPT_COOKIE_BTN));
+       WebElement acceptBtn = driver.findElement(ACCEPT_COOKIE_BTN);
+       acceptBtn.click();
+
+       LOGGER.info("Taking 3rd article");
        WebElement currentArticle = driver.findElements(ARTICLE).get(3);
 
-       // Store title - sohranitj zagolovok
+       LOGGER.info("Finding and storing title");
        String titleToCheck = currentArticle.findElement(ARTICLE_TITLE).getText();
 
-       // Store comment count - sohranitj kolicestvo komentarij
+       // Find and store comment count - sohranitj kolicestvo komentarij
        int commentCount = 0;
 
        if (!currentArticle.findElements(COMMENTS_COUNT).isEmpty()) {
